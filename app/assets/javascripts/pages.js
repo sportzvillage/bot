ddc38219967a749ea8cadf52c8020b7f2b853c20
Rohit Deshmukh <rohit.deshmukh119@gmail.com>
2016-10-17 23:12:43 +0530

    Pg gem

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,33 +1,76 @@
-
 $(function() {
-	$('#chemistry').hide();
-	$('#maths').hide();
-	$('#physics').hide();
-	$('#gravity').hide();
-  $('#span').hide();
-	
-  
+    $('#how').hide();
+    $('#lets-learn').hide();
+    $('#brazil').hide();
+    $('#portugal').hide();
+  $('#argentina').hide();
+  $('#list-of-chapters').hide();
+  $('#more-fun-facts').hide();
+  $('#inside-our-earth').hide();
+  $('#layers-of-earth').hide();
+  $('#crust').hide();
+  $('#mantle').hide();
+  $('#core').hide();
+  $('#more-metamorphic-rocks').hide();
+    $('#next-rock-type').hide();
+  $('#last-rock-type').hide();
+  $('#formation-of-rocks').hide();
+
 $( ".btn-primary" ).click(function() {
   var value = $( this ).text();
   $( this ).closest('div').hide();
   $( this ).closest('div').parent().children('span').text(value).addClass('btn btn-primary').fadeIn(2000);
-  
+ 
   switch(value){
-  	case "Chemistry":
-  		$('#chemistry').fadeIn('slow');
-  	break;
-    case "Gravity":
-      $('#gravity').fadeIn('slow');
+      case "How? (click here)":
+          $('#how').fadeIn('slow');
       break;
-  	case "Maths":
-  		$('#maths').fadeIn('slow');
+    case "Let's Learn!":
+      $('#lets-learn').fadeIn('slow');
+      break;
+      case "Brazil":
+          $('#brazil').fadeIn('slow');
     break;
-    case "Physics":
-  		$('#physics').fadeIn('slow');
+    case "Argentina":
+          $('#argentina').fadeIn('slow');
+    break;
+    case "Portugal":
+      $('#portugal').fadeIn('slow');
+    break;
+    case "List of Chapters":
+      $('#list-of-chapters').fadeIn('slow');
+    break;
+    case "More fun facts!":
+      $('#more-fun-facts').fadeIn('slow');
+    break;
+    case "Inside Our Earth":
+      $('#inside-our-earth').fadeIn('slow');
+    break;
+    case "Layers of Earth":
+      $('#layers-of-earth').fadeIn('slow');
+    break;
+    case "Crust":
+      $('#crust').fadeIn('slow');
+    break;
+    case "Mantle":
+      $('#mantle').fadeIn('slow');
+    break;
+    case "Core":
+      $('#core').fadeIn('slow');
+    break;
+    case "More Metamorphic Rocks":
+      $('#more-metamorphic-rocks').fadeIn('slow');
+    break;
+    case "Next Rock Type":
+      $('#next-rock-type').fadeIn('slow');
+    break;
+    case "Last Rock Type":
+      $('#last-rock-type').fadeIn('slow');
+    break;
+    case "Formation of Rocks":
+      $('#formation-of-rocks').fadeIn('slow');
     break;
   }
   $("html, body").animate({ scrollTop: $(document).height() }, 2000);
-});   
+});  
 })(jQuery); 
-
-
